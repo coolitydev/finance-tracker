95ea091fbba2a1003e7eae82aea2f5bb73383936
coolity <coolitydev@gmail.com>
2024-11-19 19:20:17 +0300

    Include v1 router in api router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
 from core.config import settings
+from api.v1 import router as v1_router
 
 router = APIRouter(prefix=settings.api.prefix)
+router.include_router(v1_router)
