87230ceaa7230800ef1e1bd1adaf116257b25bda
coolity <coolitydev@gmail.com>
2024-11-19 19:21:46 +0300

    Add create and get user endpoints

--- app/api/v1/user/router.py
+++ app/api/v1/user/router.py
@@ -13,3 +13,24 @@ router = APIRouter(
     prefix=settings.api.v1.user_prefix,
     tags=["Users"],
 )
+
+
+@router.post("/", response_model=UserRead)
+async def create_user(
+        user_data: UserCreate,
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    user = User(**user_data.model_dump())
+    session.add(user)
+    await session.commit()
+    return user
+
+
+@router.get("/{user_id}", response_model=Optional[UserRead])
+async def get_user(
+        user_id: int,
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)]
+):
+    stmt = select(User).where(User.id == user_id)
+    result = await session.execute(stmt)
+    return result.scalars().first()
